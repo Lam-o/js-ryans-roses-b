7b8eaa2a20c447f0dc6e77f77304477f74e87523
lam <i.mailed.lam@gmail.com>
2023-11-14 09:21:15 +0200

    R&R b

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// change color of insta and facebook icons
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+//add a border around opening hours paragraph
+var openingHours = document.querySelector(".contact p");
+openingHours.style.border = "1px solid #d45bb9";
+
+//now add padding
+openingHours.style.padding = "1em";
